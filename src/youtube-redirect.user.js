b947b5ede2adfe080ddd9ad2e61329b41e1c0bb8
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2024-06-09 16:39:22 -0700

    Build from src (#1)
    * Test SRC
    
    * Updates
    
    * Fixes

--- src/youtube-redirect.user.js
+++ src/youtube-redirect.user.js
@@ -4,8 +4,8 @@
 // @author      Shane
 // @version     1.0
 // @namespace   https://github.com/smashedr
-// @updateURL   https://github.com/smashedr/youtube-embed/raw/master/youtube-redirect.user.js
-// @icon        https://github.com/smashedr/youtube-embed/raw/master/favicon.ico
+// @updateURL   https://github.com/smashedr/youtube-embed/raw/master/src/youtube-redirect.user.js
+// @icon        https://github.com/smashedr/youtube-embed/raw/master/src/favicon.ico
 // @run-at      document-start
 // @match       https://www.youtube.com/watch?v=*
 // ==/UserScript==
